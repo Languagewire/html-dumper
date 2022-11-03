d710ca0622aff5cdcea0c557431a402b5289aa08
Daniel Sorichetti <daso@languagewire.com>
2022-11-03 16:12:52 +0100

    Add dynamic coverage status badge from Coveralls

--- build/build.sh
+++ build/build.sh
@@ -10,8 +10,3 @@ export $(grep -v '^#' .env | xargs)
 --build-arg LIBRARY_RELEASE_NAME=$LIBRARY_RELEASE_NAME \
 --build-arg RUN_INTEGRATION_TESTS=$RUN_INTEGRATION_TESTS \
 -t "$PROJECT_NAME" -f build/Dockerfile .)
-
-# Run a container to download artifacts
-docker run --name "${PROJECT_NAME}-build" "$PROJECT_NAME" /bin/true
-
-docker rm "${PROJECT_NAME}-build"
